df7c6c8bde7829932bcd2187aa297ae21adfeb63
taldomelin <gumelo.cant@gmail.com>
2023-08-01 10:03:52 -0300

    Atualizando Routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,32 @@
 <?php
 
+use App\Http\Controllers\ExercicioCincoController;
+use App\Http\Controllers\ExercicioDoisController;
+use App\Http\Controllers\ExercicioNoveController;
+use App\Http\Controllers\ExercicioOitoController;
+use App\Http\Controllers\ExercicioQuatrooController;
+use App\Http\Controllers\ExercicioSeisController;
+use App\Http\Controllers\ExercicioSeteController;
+use App\Http\Controllers\ExercicioTresController;
+use App\Http\Controllers\ExercicioUmController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('exercicio/um', [ExercicioUmController::class, 'multiplicar']);
+
+Route::post('exercicio/dois', [ExercicioDoisController::class, 'retornarMaior']);
+
+Route::get('exercicio/sete', [ExercicioSeteController::class, 'exibirNumeros']);
+
+Route::post('exercicio/quatro', [ExercicioQuatrooController::class, 'verificarNumero']);
+
+Route::post('exercicio/tres', [ExercicioTresController::class, 'mediaAritmetica']);
+
+Route::post('exercicio/cinco', [ExercicioCincoController::class, 'verificacao']);
+
+Route::post('exercicio/seis', [ExercicioSeisController::class, 'idade']);
+
+Route::post('exercicio/oito', [ExercicioOitoController::class, 'tabuada']);
+
+Route::post('exercicio/nove', [ExercicioNoveController::class, 'divisao']);
